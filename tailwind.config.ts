47d7a0edb59af22f05a88e5205a53148b99ee064
ashish-icb <ashish@indiancashback.com>
2024-09-23 10:47:24 +0530

    added pre-commit hook and some prettier and eslint scripts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,7 +74,7 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate')], //eslint-disable-line
 } satisfies Config;
 
 export default config;
